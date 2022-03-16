b1ad472d8725618c087785bc69841b677827d7cd
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-03-16 11:19:54 +0100

    job1 finis

---
+++ jour03/job01/script.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function(){
+    jQuery('#button').on('click', function() {
+        //toggle est une fonction qui permet de cacher et afficher
+        jQuery("p").toggle()
+    })
+})
