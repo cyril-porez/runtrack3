87f08fc8206be0949908ed44d1a369b7b7574662
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-03-17 13:33:56 +0100

    finis

---
+++ jour04/job01/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded',(event) =>{
+    let button = document.getElementById('button')
+    let text = document.createElement('p')
+    let main = document.querySelector('main')
+
+    button.addEventListener('click', (event) =>{
+        fetch("expression.txt")
+            .then((response) => response.text())
+            .then((txt) => {
+                text.innerHTML = txt;
+                main.append(paragraphe);
+            })
+            .catch((error) => console.log(error));
+     });
+})
